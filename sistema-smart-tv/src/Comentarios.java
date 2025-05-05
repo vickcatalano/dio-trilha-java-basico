27a1c87fe448136380eb29ea23ee65119b623cc4
vickcatalano <vickcatalano@gitbub.com>
2025-05-05 13:43:22 -0300

    criando projeto de comentarios java

---
+++ sistema-smart-tv/src/Comentarios.java
@@ -0,0 +1,24 @@
+public class Comentarios {
+ public static void main(String[] args) {
+    // Olá, eu sou um comentario de uma unica linha
+
+    /* Olá,
+    * Eu sou um comentario
+    * que posso ser mais detalhado
+    * quando necessario 
+    */
+
+ }
+    /** 
+    * Estas duas estrelinhas acima
+    * sao para identificar que você
+    * pretende elaborar um comentário 
+    * a nível de documentacao.
+    * Que incrível !!!
+    */
+
+    public void metodo()
+
+
+ }    
+}
